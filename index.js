86143549d9421d9c2ebcb42d9cf3c26fc69f336c
Suzanne Banasihan <sjb04@ymail.com>
2023-01-05 03:51:59 -0800

    question prompts for departments, roles, and employees

--- index.js
+++ index.js
@@ -101,3 +101,84 @@ db.query("SELECT employees.id, employees.first_name, employees.last_name, roles.
     }
 });
 };
+
+// prompts questions about department
+const addDepartment = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newDepartment',
+            message: 'What department would you like to add?'
+        }
+    ]).then((data) => {
+        db.query('INSERT INTO departments(department_name) VALUES (?)', data.newDepartment, async function (err, results) {
+            await console.log(`${data.newDepartment} added to departments!`);
+            tracker();
+        } );
+    });
+};
+
+// prompts questions about role
+const addRole = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'newRole',
+            message: 'What is the title of the role would you like to add?'
+        },
+        {
+            type: 'input',
+            name: 'salary',
+            message: 'What is the salary of this role?'
+        },
+        {
+            type: 'input',
+            name: 'department',
+            message: 'what is the department id for this role?'
+        }
+    ]).then((data) => {
+        db.query('INSERT INTO roles(title, salary, department_id) VALUES (?,?,?)', [data.newRole, data.salary, data.department], async function (err, results) {
+            await console.log(`${data.newRole} added to roles!`);
+            tracker();
+        } );
+    });
+};
+
+// prompts questions about employee
+const addEmployee = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'firstName',
+            message: 'What is the first name of the employee you would like to add?'
+        },
+        {
+            type: 'input',
+            name: 'lastName',
+            message: 'What is the last name of the employee you would like to add?'
+        },
+        {
+            type: 'input',
+            name: 'role',
+            message: 'what is the role id for this employee?'
+        },
+        {
+            type: 'input',
+            name: 'manager',
+            message: 'What is the manager id for this employee ? (if applicable)'
+        }
+    ]).then((data) => {
+        db.query('INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)', [data.firstName, data.lastName, data.role, data.manager], async function (err, results) {
+            await console.log(`${data.firstName} added to roles!`);
+            tracker();
+        } );
+    });
+};
+
+const viewRoleByManager = () => {
+
+};
+
+const viewEmployeesByDepartment = () => {
+db.query('SELECT ')
+};
