c258648b8e765e7172649dde2ed0e2868d1c35f6
Suzanne Banasihan <sjb04@ymail.com>
2023-01-05 01:58:37 -0800

    added roles and condensed departments

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,12 +3,16 @@ VALUES  ("Sales")
         ("Legal")
         ("Finance")
         ("Engineering")
-        ("Human Resources")
-        ("Production")
-        ("Customer Service")
-
-INSERT INTO
-VALUES
-
+       
+INSERT INTO roles (title, salary, department_id)
+VALUES  ("Sales Lead", 120000, 1)
+        ("Salesperson", 95000, 1)
+        ("Lawyer", 200000, 2)
+        ("Administrative Assistant", 60000, 2)
+        ("Account Manager", 150000, 3)
+        ("Accountant", 115000, 3)
+        ("Sr. Engineer", 180000, 4)
+        ("Jr. Engineer", 130000, 4)
+        
 INSERT INTO
 VALUES
