08cc0dc69295b7874d6612bd0e0532f0433b00ee
Suzanne Banasihan <sjb04@ymail.com>
2023-01-04 23:26:54 -0800

    created table for employees

--- db/schema.sql
+++ db/schema.sql
@@ -17,3 +17,12 @@ CREATE TABLE roles (
     REFERENCES departments(id)
     ON DELETE SET NULL
 );
+
+CREATE TABLE employees (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL
+);
